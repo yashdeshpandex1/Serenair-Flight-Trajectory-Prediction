6ab07ec7831100c397701a1d9c5d3cdfcfb0c311
unknown <yashdeshpandex1@gmail.com>
2026-04-06 22:31:37 +0530

    feat: Clean the data with most app. methods.

--- preprocessing/data_cleaning.py
+++ preprocessing/data_cleaning.py
@@ -3,3 +3,21 @@
 # Imports
 import numpy as np
 import pandas as pd
+# Import sorting function from utilties
+from utilities import sort_values
+
+df = pd.read_csv('../data/opensky_raw.csv') # initialise df
+
+df_sorted = sort_values(df) # Sort by icao24 and timestamp
+
+# Clean the dataframe
+def clean_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the OpenSky Dataset with most appropriate methods.
+    """
+    df = df.dropna(subset=['latitude', 'longitude']) # drop the rows where latitude and longitude are missing
+    df.loc[:, 'baro_altitude'] = df['baro_altitude'].fillna(df['baro_altitude'].median()) # fill baro altitude missing values with median
+    df.loc[:, 'velocity'] = df['velocity'].fillna(df['velocity'].median()) # fill missing velocity with median as well
+    df.loc[:, 'vertical_rate'] = df['vertical_rate'].fillna(value=0) # fill vertical rate with 0s
+    df = df.drop(columns=['callsign', 'geo_altitude']) # we don't really need these columns for our application
+    return df
