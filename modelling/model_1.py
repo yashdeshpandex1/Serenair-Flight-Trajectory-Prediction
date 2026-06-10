d6daf29c6f4fb3471155a82ac328732d79789186
unknown <yashdeshpandex1@gmail.com>
2026-06-10 18:17:41 +0530

    Fix: refactor model_1 code - Better documentation and clarity

--- modelling/model_1.py
+++ modelling/model_1.py
@@ -1,36 +1,35 @@
 import torch
 import torch.nn as nn
 
-class model1(nn.Module):
-    """A baseline lstm model._
-    """
-    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
-        super(model1, self).__init__()
+class LSTMModelV1(nn.Module):
+    """A simple one layer LSTM model."""
+    def __init__(self, input_size, hidden_size,
+                 output_size, num_layers=1):
+        super(LSTMModelV1, self).__init__()
+        
+        # Model specifications
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
         # LSTM Layer
-        self.lstm = nn.LSTM(input_size = input_size,
-                            hidden_size = hidden_size,
-                            num_layers = num_layers,
-                            batch_first = True)
+        self.lstm = nn.LSTM(input_size=input_size,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True)
         
-        # Output Layer
+        # Fully connected Layer
         self.fc = nn.Linear(hidden_size, output_size)
         
-        
-        
     def forward(self, X):
-        """
-        X: input of shape (batch_size, seq_length, input_size).
-        """
+        """X: input of shape (batch_size, seq_length, input_size)."""
+        
         # LSTM returns (output, (h_n, c_n))
         lstm_out, (h_n, c_n) = self.lstm(X)
         
-        # Take the output from last time step
+        # Take the output from the last time step
         last_output = lstm_out[:, -1, :]
         
-        # Pass through output layer
+        # finally, pass it through the fully connected layer
         output = self.fc(last_output)
         
         return output
