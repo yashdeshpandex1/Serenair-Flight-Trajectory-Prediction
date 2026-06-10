1bc6e73950d0f50572d1d07339a869b2e209c438
unknown <yashdeshpandex1@gmail.com>
2026-06-11 01:08:00 +0530

    feat: HybridConvLSTMV3 - A Hybrid Conv + LSTM model with two conv and fc layers, leaky relu and dropouts

--- modelling/model_7.py
+++ modelling/model_7.py
@@ -1,39 +1,47 @@
 import torch
 import torch.nn as nn
 
-class HybridConvLSTMBidirectionalModel(nn.Module):
-    """A Hybrid (Conv + Bidirectional LSTM) Model with 
-        two fully connected layers and a skip connection."""
+class HybridConvLSTMV3(nn.Module):
+    """A Hybrid Conv + LSTM Layer model with two fully connected
+       layer and skip connection."""
     def __init__(self, input_size, hidden_size,
                  output_size, num_layers=2, dropout_rate=0.2):
-        super(HybridConvLSTMBidirectionalModel, self).__init__()
+        super(HybridConvLSTMV3, self).__init__()
         
-        # Conv Layer
+        # Conv1 Layer
         self.conv1 = nn.Conv1d(in_channels=input_size,
                                out_channels=32,
                                kernel_size=3,
                                padding=1)
-        self.relu = nn.ReLU() # relu activation
-        self.dropout = nn.Dropout(dropout_rate) # dropout
+        # Conv2 Layer
+        self.conv2 = nn.Conv1d(in_channels=32, 
+                               out_channels=32,
+                               kernel_size=3, 
+                               padding=1)
+        self.leaky_relu = nn.LeakyReLU() # Leaky relu activation
+        
+        self.spatial_dropout = nn.Dropout1d(dropout_rate)   # Use spatial dropout for conv layers
+        self.dropout = nn.Dropout(dropout_rate)     # standard dropout for fc layers
         self.skip_projector = nn.Linear(input_size, 32) # skip connection
         
         # LSTM Layer
         self.lstm = nn.LSTM(input_size=32,
-                            hidden_size=hidden_size,
+                            hidden_size=hidden_size, 
                             num_layers=num_layers,
                             batch_first=True,
-                            bidirectional=True,
                             dropout=dropout_rate if num_layers > 1 else 0.0)
         
-        self.fc1 = nn.Linear(hidden_size * 2, 64)   # fully connected layer 1
-        self.fc2 = nn.Linear(64, output_size)   # fully connected layer 2
+        self.fc1 = nn.Linear(hidden_size, 64) # fully connected layer 1
+        self.fc2 = nn.Linear(64, output_size) # fully connected layer 2
         
     def forward(self, X):
         """X: input of shape (batch_size, seq_length, input_size)."""
         
         # permute X shape to (batch_size, input_size, seq_length)
         x_cnn = X.permute(0, 2, 1)
-        x_cnn = self.relu(self.conv1(x_cnn))    # apply relu activation
+        x_cnn = self.leaky_relu(self.conv1(x_cnn)) # Conv layer and leaky relu activation
+        x_cnn = self.spatial_dropout(x_cnn) # Spatial dropout for conv layer
+        x_cnn = self.leaky_relu(self.conv2(x_cnn)) # Conv layer and leaky relu 
         
         # permute X shape back to (batch_size, seq_length, input_size)
         x_rnn = x_cnn.permute(0, 2, 1)
@@ -41,19 +49,15 @@ class HybridConvLSTMBidirectionalModel(nn.Module):
         x_combined = self.dropout(x_rnn + skip) # combine skip connection & x_rnn
         
         # LSTM returns (output, (h_n, c_n))
-        lstm_out, (h_n, _) = self.lstm(x_combined)
+        lstm_out, _ = self.lstm(x_combined)
         
-        # Take mature outputs from both forward and backward pass (bidirectional)
-        forward_hidden = h_n[-2, :, :]
-        backward_hidden = h_n[-1, :, :]
+        # Take the output from the last time step
+        last_output = lstm_out[:, -1, :]
         
-        # Concat forward and backward output to get the output
-        last_output = torch.cat((forward_hidden, backward_hidden), dim=1)
-        
-        # Conv Layer -> relu -> skip connection -> 
-        # Bidirectional LSTM Layers * 2 -> fc1 -> relu -> dropout -> 
-        # fc2 -> output
-        x = self.dropout(self.relu(self.fc1(last_output)))
+        # Conv Layer 1 -> leaky relu -> dropout -> conv 2 layer -> leaky relu ->
+        # skip connection -> LSTM Layers * 2 -> fc1 -> leaky relu -> dropout ->
+        # fc2 -> output 
+        x = self.dropout(self.leaky_relu(self.fc1(last_output)))
         output = self.fc2(x)
         
-        return output
+        return output 
--- modelling/model_8.py
+++ modelling/model_8.py
@@ -1,60 +1,59 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class TemporalAttention(nn.Module):
-    """Attention block to get context vector and attention weights."""
-    def __init__(self, hidden_size):
-        super(TemporalAttention, self).__init__()
-        self.attention_scorer = nn.Linear(hidden_size, 1)
-        
-    def forward(self, rnn_output):
-        # get attention scores from rnn output
-        scores = self.attention_scorer(rnn_output)
-        
-        # use softmax to get attention weights
-        attn_weights = F.softmax(scores, dim=1)
-        
-        # multiply weights with rnn outputs to get context vectors
-        context_vector = torch.sum(attn_weights * rnn_output, dim=1)
-        
-        # return context vector and attention weights
-        return context_vector, attn_weights
-    
-class AttentionModel(nn.Module):
-    """An Attention model with two GRU Layers and two fully connected layers."""
+class HybridConvLSTMBidirectionalModel(nn.Module):
+    """A Hybrid (Conv + Bidirectional LSTM) Model with 
+        two fully connected layers and a skip connection."""
     def __init__(self, input_size, hidden_size,
                  output_size, num_layers=2, dropout_rate=0.2):
-        super(AttentionModel, self).__init__()
+        super(HybridConvLSTMBidirectionalModel, self).__init__()
         
-        # GRU Layer
-        self.gru = nn.GRU(input_size=input_size,
-                          hidden_size=hidden_size,
-                          num_layers=num_layers,
-                          batch_first=True,
-                          dropout=dropout_rate if num_layers > 1 else 0.0)
+        # Conv Layer
+        self.conv1 = nn.Conv1d(in_channels=input_size,
+                               out_channels=32,
+                               kernel_size=3,
+                               padding=1)
         self.relu = nn.ReLU() # relu activation
         self.dropout = nn.Dropout(dropout_rate) # dropout
+        self.skip_projector = nn.Linear(input_size, 32) # skip connection
         
-        # Attention Layer
-        self.attention = TemporalAttention(hidden_size)
+        # LSTM Layer
+        self.lstm = nn.LSTM(input_size=32,
+                            hidden_size=hidden_size,
+                            num_layers=num_layers,
+                            batch_first=True,
+                            bidirectional=True,
+                            dropout=dropout_rate if num_layers > 1 else 0.0)
         
-        self.fc1 = nn.Linear(hidden_size, 64) # fully connected layer 1
-        self.fc2 = nn.Linear(64, output_size) # fully connected layer 2
+        self.fc1 = nn.Linear(hidden_size * 2, 64)   # fully connected layer 1
+        self.fc2 = nn.Linear(64, output_size)   # fully connected layer 2
         
     def forward(self, X):
         """X: input of shape (batch_size, seq_length, input_size)."""
         
-        # GRU returns (output, h_n)
-        gru_out, _ = self.gru(X)
+        # permute X shape to (batch_size, input_size, seq_length)
+        x_cnn = X.permute(0, 2, 1)
+        x_cnn = self.relu(self.conv1(x_cnn))    # apply relu activation
         
-        # Get context vector and attention weights from GRU Layer
-        context_vector, attn_weights = self.attention(gru_out)
+        # permute X shape back to (batch_size, seq_length, input_size)
+        x_rnn = x_cnn.permute(0, 2, 1)
+        skip = self.skip_projector(X)   # skip connection
+        x_combined = self.dropout(x_rnn + skip) # combine skip connection & x_rnn
         
-        # GRU Layers * 2 -> attention layer -> fc1 -> relu -> dropout ->
+        # LSTM returns (output, (h_n, c_n))
+        lstm_out, (h_n, _) = self.lstm(x_combined)
+        
+        # Take mature outputs from both forward and backward pass (bidirectional)
+        forward_hidden = h_n[-2, :, :]
+        backward_hidden = h_n[-1, :, :]
+        
+        # Concat forward and backward output to get the output
+        last_output = torch.cat((forward_hidden, backward_hidden), dim=1)
+        
+        # Conv Layer -> relu -> skip connection -> 
+        # Bidirectional LSTM Layers * 2 -> fc1 -> relu -> dropout -> 
         # fc2 -> output
-        x = self.dropout(self.relu(self.fc1(context_vector)))
+        x = self.dropout(self.relu(self.fc1(last_output)))
         output = self.fc2(x)
         
-        # return output and attention weights
-        return output, attn_weights
+        return output
