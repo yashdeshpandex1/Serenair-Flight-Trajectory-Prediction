77808c92e3c77536aaacad239a5ca069f2069080
unknown <yashdeshpandex1@gmail.com>
2026-06-22 01:14:46 +0530

    feat: refactor preprocessing scripts for new task next_ten_mins prediction

--- preprocessing/data_prep.py
+++ preprocessing/data_prep.py
@@ -8,76 +8,95 @@ from feature_engineering import feature_engineering
 from sklearn_utilities import group_shuffle_split, scale_dataset 
 from utilities import sort_values, num_aircrafts, \
     initialise_features_and_target, initialise_df, integrate_weather_data
-from rnn_sequences import save_sequences, save_test_sequences
+from rnn_sequences import save_sequences
 from fetch_weather_data import get_weather_data
 import argparse
 import pandas as pd
 
-def prep_train_data(path='../data/opensky_raw.csv'):
-    save_dir = Path('../data/rnn_data/') # define where feature scaler will be saved
-    save_dir.mkdir(parents=True, exist_ok=True) # Create the folder if missing
-    df = initialise_df(path) # read csv file
-    df_sorted = sort_values(df) # sort the dataframe
-    df_clean = clean_df(df_sorted) # clean the dataframe
-    df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
-    
-    center_lat = df_clean['latitude'].mean()
-    center_lon = df_clean['longitude'].mean()
-    start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
-    end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
-    
-    df_weather = get_weather_data(center_lat, center_lon,
-                                  start_date, end_date)
-    df_clean = integrate_weather_data(df_clean, df_weather)
-    
-    df_engineered = feature_engineering(df_clean) # feature engineering
-    features, target = initialise_features_and_target() # features and target
+def prep_train_data(path='../data/opensky_raw.csv', task='next_instance'):
+    """Complete data pipeline to from feature engineering to creating sequences,
+    collecting and integrating weather data, shuffle split and saving scaler files.
 
-    training_set, testing_set = group_shuffle_split(df=df_engineered) # shuffle split
-    df_train, df_test, feature_scaler, target_scaler = scale_dataset(training_set, testing_set, features, target) # dataset scaling
+    Args:
+        path (str): File needed to process. Defaults to '../data/opensky_raw.csv'.
+        task (str): Choosing prediction task. Defaults to 'next_instance'.
+    """
+    if task=='next_instance':
+        save_dir = Path('../data/rnn_data_next_instance/') # define where feature scaler will be saved
+        save_dir.mkdir(parents=True, exist_ok=True) # Create the folder if missing
+        df = initialise_df(path) # read csv file
+        df_sorted = sort_values(df) # sort the dataframe
+        df_clean = clean_df(df_sorted) # clean the dataframe
+        df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
+        
+        center_lat = df_clean['latitude'].mean()
+        center_lon = df_clean['longitude'].mean()
+        start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
+        end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+        
+        df_weather = get_weather_data(center_lat, center_lon,
+                                    start_date, end_date)
+        df_clean = integrate_weather_data(df_clean, df_weather)
+        
+        df_engineered = feature_engineering(df_clean) # feature engineering
+        features, target = initialise_features_and_target() # features and target
 
-    save_sequences(df_train, df_test, features, target)
-    
-    scaler_path = save_dir/'feature_scaler.joblib'
-    target_path = save_dir/'target_scaler.joblib'
-    
-    joblib.dump(feature_scaler, scaler_path)
-    joblib.dump(target_scaler, target_path)
+        training_set, testing_set = group_shuffle_split(df=df_engineered) # shuffle split
+        df_train, df_test, feature_scaler, target_scaler = scale_dataset(training_set, testing_set, features, target) # dataset scaling
 
-def prep_test_data(path='../data/opensky_test.csv'):
-    save_dir = Path('../data/rnn_data/')
-    save_dir.mkdir(parents=True, exist_ok=True)
-    df_test = initialise_df(path)
-    df_test = sort_values(df_test)
-    df_test = clean_df(df_test)
-    df_test = feature_engineering(df_test)
+        save_sequences(df_train, df_test, features, target, task='next_instance')
+        
+        scaler_path = save_dir/'feature_scaler_next_instance.joblib'
+        target_path = save_dir/'target_scaler_next_instance.joblib'
+        
+        joblib.dump(feature_scaler, scaler_path)
+        joblib.dump(target_scaler, target_path)
+        
+    elif task=='next_ten_mins':
+        save_dir = Path('../data/rnn_data_next_ten_mins')
+        save_dir.mkdir(parents=True, exist_ok=True)
+        
+        df = initialise_df(path)
+        df_sorted = sort_values(df)
+        df_clean = clean_df(df_sorted)
+        df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
+        
+        center_lat = df_clean['latitude'].mean()
+        center_lon = df_clean['longitude'].mean()
+        start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
+        end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+        
+        df_weather = get_weather_data(center_lat, center_lon,
+                                      start_date, end_date)
+        df_clean = integrate_weather_data(df_clean, df_weather)
+        
+        df_engineered = feature_engineering(df_clean)
+        features, target = initialise_features_and_target()
+        
+        training_set, testing_set = group_shuffle_split(df=df_engineered)
+        df_train, df_test, feature_scaler, target_scaler = scale_dataset(
+            training_set, testing_set, features, target
+        )
+        
+        save_sequences(df_train, df_test, features, target, task='next_ten_mins')
+        scaler_path = save_dir/'feature_scaler_next_ten_mins.joblib'
+        target_path = save_dir/'target_scaler_next_ten_mins.joblib'
+        
+        joblib.dump(feature_scaler, scaler_path)
+        joblib.dump(target_scaler, target_path)
     
-    features, target = initialise_features_and_target()
-    df_test['raw_latitude'] = df_test['latitude']
-    df_test['raw_longitude'] = df_test['longitude']
-    
-    scaler_path = save_dir / 'feature_scaler.joblib'
-    target_path = save_dir / 'target_scaler.joblib'
-    
-    feature_scaler = joblib.load(scaler_path)
-    target_scaler = joblib.load(target_path)
-    
-    df_test[features] = feature_scaler.transform(df_test[features])
-    df_test[target] = target_scaler.transform(df_test[target])
-    
-    save_test_sequences(df_test, features, target, window_size=10)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run data pipelines')
-    parser.add_argument('--mode', type=str, required=True, choices=['train', 'test'],
-                        help="Which pipeline to run: 'train' or 'test'?")
+    parser.add_argument('--task', type=str, required=True, choices=['next_instance', 'next_ten_mins'],
+                        help="Which pipeline to run: 'next instance?' or 'next ten mins?'?")
     parser.add_argument('--file', type=str, default=None,
                         help='Path to the CSV file (optional)')
     args = parser.parse_args()
 
-    if args.mode == 'train':
+    if args.task == 'next_instance':
         filepath = args.file if args.file else '../data/opensky_raw.csv'
-        prep_train_data(path=filepath)
-    elif args.mode == 'test':
-        filepath = args.file if args.file else '../data/opensky_test.csv'
-        prep_test_data(path=filepath)
+        prep_train_data(path=filepath, task='next_instance')
+    elif args.task == 'next_ten_mins':
+        filepath = args.file if args.file else '../data/opensky_raw.csv'
+        prep_train_data(path=filepath, task='next_ten_mins')
--- preprocessing/rnn_sequences.py
+++ preprocessing/rnn_sequences.py
@@ -2,103 +2,164 @@ from tqdm import tqdm
 import numpy as np
 from pathlib import Path
 
-def create_sequences(flight_X, flight_y, flight_anchor, window_size=10):
+def create_sequences(flight_X, flight_y, flight_anchor, window_size=10,
+                     task='next_instance'):
     """
     Slide a window over a single flight's data,
-    making it appropriate for our future RNN model.
+    making it appropriate for our future RNN models.
     anchor is for reference to absolute coordinates (
         this is important because we are not predicting 
         absolute but delta coordinates
     )
     """
     X_seq, y_seq, anchor_seq = [], [], []
+    if task == 'next_instance':
+        for i in range(len(flight_X) - window_size):    # sliding window
+            X_seq.append(flight_X[i: i + window_size]) # [0:10, 1:11, 2:12 and so on...]
+            y_seq.append(flight_y[i + window_size]) 
+            last_step_idx = i + window_size - 1 # 9th index in 0:10
+            anchor_seq.append(flight_anchor[last_step_idx]) # add the last coordinate to anchor 
+
+        return np.array(X_seq), np.array(y_seq), np.array(anchor_seq)
     
-    for i in range(len(flight_X) - window_size):
-        X_seq.append(flight_X[i: i + window_size]) # [0:10, 10:20 and so on...]
-        y_seq.append(flight_y[i + window_size]) 
-        last_step_idx = i + window_size - 1 # 9th index in 0:10
-        anchor_seq.append(flight_anchor[last_step_idx])
+    elif task == 'next_ten_mins':
+        steps_ahead = 60    # skip 60 secs
         
-    return np.array(X_seq), np.array(y_seq), np.array(anchor_seq)
+        if len(flight_X) < (window_size + steps_ahead): # if len is less than 70, dump it
+            return np.array([]), np.array([]), np.array([])    
+        
+        for i in range(len(flight_X) - window_size - steps_ahead + 1):  #sliding window
+            X_seq.append(flight_X[i: i + window_size])  # [0:10, 1:11, 2:12 and so on...]
+            last_step_idx = i + window_size -1 # 9th index in 0:10
+            future_targets = [] # make a list of y (10 instances of (lat, lon))
+            for minute in range(1, 11):
+                target_idx = last_step_idx + (minute * 6)
+                future_targets.append(flight_y[target_idx]) # add to the future targets list
+                
+            y_seq.append(future_targets)
+            anchor_seq.append(flight_anchor[last_step_idx])
+            
+        return np.array(X_seq), np.array(y_seq), np.array(anchor_seq)
 
 
-def prepare_rnn_data(df, features, target, anchor_cols, window_size=10):
+def prepare_rnn_data(df, features, target, anchor_cols, window_size=10,
+                     task='next_instance'):
     """
     Builds up on create_sequences function to further
     create the desired RNN dataset for our models.
     """
     all_X, all_y, all_anchors = [], [], []
     
-    for _, flight_data in tqdm(df.groupby('icao24'), desc='Extracting Flight Sequences'):
+    if task == 'next_instance':
+        print(f"Creating sequences for next instance prediction.")
+        for _, flight_data in tqdm(df.groupby('icao24'), desc='Extracting Flight Sequences (next_instance)'):
+            flight_data = flight_data.sort_values(by='timestamp')
+
+            flight_X = flight_data[features].values
+            flight_y = flight_data[target].values
+            flight_anchor = flight_data[anchor_cols].values
+
+            if len(flight_X) > window_size:
+                X_s, y_s, anchor_s = create_sequences(flight_X, flight_y, flight_anchor, window_size=window_size, task='next_instance')
+                
+                if X_s.size > 0:
+                    all_X.append(X_s)
+                    all_y.append(y_s)
+                    all_anchors.append(anchor_s)
+                    
+        if not all_X:
+            return np.array([]), np.array([]), np.array([])
         
-        flight_data = flight_data.sort_values(by='timestamp')
-        
-        flight_X = flight_data[features].values
-        flight_y = flight_data[target].values
-        flight_anchor = flight_data[anchor_cols].values
-        
-        if len(flight_X) > window_size:
-            X_s, y_s, anchor_s = create_sequences(flight_X, flight_y, flight_anchor, window_size=window_size)
-            all_X.append(X_s)
-            all_y.append(y_s)
-            all_anchors.append(anchor_s)
+        # return X, y and anchors        
+        return np.vstack(all_X), np.vstack(all_y), np.vstack(all_anchors)
+    
+    elif task == 'next_ten_mins':
+        print(f"Creating sequences for next ten minutes prediction.")
+        for _, flight_data in tqdm(df.groupby('icao24'), desc='Extracting Flight Sequences (next_ten_mins)'):
             
-    # return X, y and anchors        
-    return np.vstack(all_X), np.vstack(all_y), np.vstack(all_anchors)
+            flight_data = flight_data.sort_values(by='timestamp')
+            
+            flight_X = flight_data[features].values
+            flight_y = flight_data[target].values
+            flight_anchor = flight_data[anchor_cols].values
+            
+            X_s, y_s, anchor_s = create_sequences(flight_X, flight_y, flight_anchor, window_size=10, task='next_ten_mins')
+            
+            if X_s.size > 0:
+                all_X.append(X_s)
+                all_y.append(y_s)
+                all_anchors.append(anchor_s)
+                
+        if not all_X:
+            return np.array([]), np.array([]), np.array([])
+        
+        return np.vstack(all_X), np.vstack(all_y), np.vstack(all_anchors)
+        
 
-
-def save_sequences(train_df, test_df, features, target, window_size=10):
+def save_sequences(train_df, test_df, features, target, window_size=10,
+                   task='next_instance'):
     
     anchor_cols = ['raw_latitude', 'raw_longitude']
-    
-    X_train_seq, y_train_seq, anchor_train_seq = prepare_rnn_data(
-        df=train_df,
-        features=features,
-        target=target,
-        anchor_cols=anchor_cols,
-        window_size=window_size
-    )
-    
-    X_test_seq, y_test_seq, anchor_test_seq = prepare_rnn_data(
-        df=test_df,
-        features=features,
-        target=target,
-        anchor_cols=anchor_cols,
-        window_size=window_size
-    )
-    
-    data_dir = Path('../data/rnn_data/')
-    data_dir.mkdir(parents=True, exist_ok=True)
-    save_path = data_dir / 'flight_data_for_rnn.npz'
-    
-    np.savez_compressed(save_path,
-                        X_train = X_train_seq,
-                        y_train = y_train_seq,
-                        anchor_train = anchor_train_seq,
-                        X_test = X_test_seq,
-                        y_test = y_test_seq,
-                        anchor_test = anchor_test_seq)
-    print('Data saved successfully..')
+    if task == 'next_instance':
+        X_train_seq, y_train_seq, anchor_train_seq = prepare_rnn_data(
+            df=train_df,
+            features=features,
+            target=target,
+            anchor_cols=anchor_cols,
+            window_size=window_size,
+            task='next_instance'
+        )
+        
+        X_test_seq, y_test_seq, anchor_test_seq = prepare_rnn_data(
+            df=test_df,
+            features=features,
+            target=target,
+            anchor_cols=anchor_cols,
+            window_size=window_size,
+            task='next_instance'
+        )
+        
+        data_dir = Path('../data/rnn_data_next_instance/')
+        data_dir.mkdir(parents=True, exist_ok=True)
+        save_path = data_dir / 'flight_data_for_rnn_next_instance.npz'
+        
+        np.savez_compressed(save_path,
+                            X_train = X_train_seq,
+                            y_train = y_train_seq,
+                            anchor_train = anchor_train_seq,
+                            X_test = X_test_seq,
+                            y_test = y_test_seq,
+                            anchor_test = anchor_test_seq)
+        print('Data saved successfully (next instance)..')
+    elif task == 'next_ten_mins':
+        X_train_seq, y_train_seq, anchor_train_seq = prepare_rnn_data(
+            df=train_df,
+            features=features,
+            target=target,
+            anchor_cols=anchor_cols,
+            window_size=window_size,
+            task='next_ten_mins'
+        )
+        
+        X_test_seq, y_test_seq, anchor_test_seq = prepare_rnn_data(
+            df=test_df,
+            features=features,
+            target=target,
+            anchor_cols=anchor_cols,
+            window_size=window_size,
+            task='next_ten_mins'
+        )
+        
+        data_dir = Path('../data/rnn_data_next_ten_mins/')
+        data_dir.mkdir(parents=True, exist_ok=True)
+        save_path = data_dir / 'flight_data_for_rnn_next_ten_mins.npz'
+        
+        np.savez_compressed(save_path,
+                            X_train = X_train_seq,
+                            y_train = y_train_seq,
+                            anchor_train = anchor_train_seq,
+                            X_test = X_test_seq,
+                            y_test = y_test_seq,
+                            anchor_test = anchor_test_seq)
+        print('Data saved successfully (next ten minutes)..')
 
-def save_test_sequences(test_df, features, target, window_size=10):
-    
-    anchor_cols = ['raw_latitude', 'raw_longitude']
-    
-    
-    X_test_seq, y_test_seq, anchor_test_seq = prepare_rnn_data(
-        df=test_df,
-        features=features,
-        target=target,
-        anchor_cols=anchor_cols,
-        window_size=window_size
-    )
-    
-    data_dir = Path('../data/rnn_data/')
-    data_dir.mkdir(parents=True, exist_ok=True)
-    save_path = data_dir / 'test_data.npz'
-    
-    np.savez_compressed(save_path,
-                        X_test = X_test_seq,
-                        y_test = y_test_seq,
-                        anchor_test = anchor_test_seq)
-    print('Data saved successfully..')
