5356cb52e5acfbe3b80bb09c45d1ea4a137b88e9
unknown <yashdeshpandex1@gmail.com>
2026-06-22 16:35:47 +0530

    fix: refactor data_prep.py

--- preprocessing/data_prep.py
+++ preprocessing/data_prep.py
@@ -21,69 +21,36 @@ def prep_train_data(path='../data/opensky_raw.csv', task='next_instance'):
         path (str): File needed to process. Defaults to '../data/opensky_raw.csv'.
         task (str): Choosing prediction task. Defaults to 'next_instance'.
     """
-    if task=='next_instance':
-        save_dir = Path('../data/rnn_data_next_instance/') # define where feature scaler will be saved
-        save_dir.mkdir(parents=True, exist_ok=True) # Create the folder if missing
-        df = initialise_df(path) # read csv file
-        df_sorted = sort_values(df) # sort the dataframe
-        df_clean = clean_df(df_sorted) # clean the dataframe
-        df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
-        
-        center_lat = df_clean['latitude'].mean()
-        center_lon = df_clean['longitude'].mean()
-        start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
-        end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
-        
-        df_weather = get_weather_data(center_lat, center_lon,
-                                    start_date, end_date)
-        df_clean = integrate_weather_data(df_clean, df_weather)
-        
-        df_engineered = feature_engineering(df_clean) # feature engineering
-        features, target = initialise_features_and_target() # features and target
+    
+    save_dir = Path(f'../data/rnn_data_{task}/') # define where feature scaler will be saved
+    save_dir.mkdir(parents=True, exist_ok=True) # Create the folder if missing
+    df = initialise_df(path) # read csv file
+    df_sorted = sort_values(df) # sort the dataframe
+    df_clean = clean_df(df_sorted) # clean the dataframe
+    df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
+    
+    center_lat = df_clean['latitude'].mean()
+    center_lon = df_clean['longitude'].mean()
+    start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
+    end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+    
+    df_weather = get_weather_data(center_lat, center_lon,
+                                start_date, end_date)
+    df_clean = integrate_weather_data(df_clean, df_weather)
+    
+    df_engineered = feature_engineering(df_clean) # feature engineering
+    features, target = initialise_features_and_target() # features and target
 
-        training_set, testing_set = group_shuffle_split(df=df_engineered) # shuffle split
-        df_train, df_test, feature_scaler, target_scaler = scale_dataset(training_set, testing_set, features, target) # dataset scaling
+    training_set, testing_set = group_shuffle_split(df=df_engineered) # shuffle split
+    df_train, df_test, feature_scaler, target_scaler = scale_dataset(training_set, testing_set, features, target) # dataset scaling
 
-        save_sequences(df_train, df_test, features, target, task='next_instance')
-        
-        scaler_path = save_dir/'feature_scaler_next_instance.joblib'
-        target_path = save_dir/'target_scaler_next_instance.joblib'
-        
-        joblib.dump(feature_scaler, scaler_path)
-        joblib.dump(target_scaler, target_path)
-        
-    elif task=='next_ten_mins':
-        save_dir = Path('../data/rnn_data_next_ten_mins')
-        save_dir.mkdir(parents=True, exist_ok=True)
-        
-        df = initialise_df(path)
-        df_sorted = sort_values(df)
-        df_clean = clean_df(df_sorted)
-        df_clean['timestamp'] = pd.to_datetime(df_clean['timestamp'], unit='s')
-        
-        center_lat = df_clean['latitude'].mean()
-        center_lon = df_clean['longitude'].mean()
-        start_date = df_clean['timestamp'].min().strftime('%Y-%m-%d')
-        end_date = (df_clean['timestamp'].max() + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
-        
-        df_weather = get_weather_data(center_lat, center_lon,
-                                      start_date, end_date)
-        df_clean = integrate_weather_data(df_clean, df_weather)
-        
-        df_engineered = feature_engineering(df_clean)
-        features, target = initialise_features_and_target()
-        
-        training_set, testing_set = group_shuffle_split(df=df_engineered)
-        df_train, df_test, feature_scaler, target_scaler = scale_dataset(
-            training_set, testing_set, features, target
-        )
-        
-        save_sequences(df_train, df_test, features, target, task='next_ten_mins')
-        scaler_path = save_dir/'feature_scaler_next_ten_mins.joblib'
-        target_path = save_dir/'target_scaler_next_ten_mins.joblib'
-        
-        joblib.dump(feature_scaler, scaler_path)
-        joblib.dump(target_scaler, target_path)
+    save_sequences(df_train, df_test, features, target, task=task)
+    
+    scaler_path = save_dir/f'feature_scaler_{task}.joblib'
+    target_path = save_dir/f'target_scaler_{task}.joblib'
+    
+    joblib.dump(feature_scaler, scaler_path)
+    joblib.dump(target_scaler, target_path)
     
     
 if __name__ == "__main__":
@@ -93,10 +60,5 @@ if __name__ == "__main__":
     parser.add_argument('--file', type=str, default=None,
                         help='Path to the CSV file (optional)')
     args = parser.parse_args()
-
-    if args.task == 'next_instance':
-        filepath = args.file if args.file else '../data/opensky_raw.csv'
-        prep_train_data(path=filepath, task='next_instance')
-    elif args.task == 'next_ten_mins':
-        filepath = args.file if args.file else '../data/opensky_raw.csv'
-        prep_train_data(path=filepath, task='next_ten_mins')
+    filepath = args.file if args.file else '../data/opensky_raw.csv'
+    prep_train_data(path=filepath, task=args.task)
